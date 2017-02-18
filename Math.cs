2ce09ab51aee98f7bbc53dd6447bdb939a87b5d8
mohs <morshedi.mohsen@gmail.com>
2017-02-18 17:56:22 -0500

    adding class for the module

--- Math.cs
+++ Math.cs
@@ -1,4 +1,7 @@
-public static int Mult(int x,int y) { return x * y; } 
-public static int Add(int x,int y) { return x + y; } 
-public static int Divide(int x,int y) { return x / y; } 
-public static int Abs(int x) { return x  ? x : -x; } 
+public static class MathModule
+{  
+	public static int Mult(int x,int y) { return x * y; } 
+	public static int Add(int x,int y) { return x + y; } 
+	public static int Divide(int x,int y) { return x / y; } 
+	public static int Abs(int x) { return x  ? x : -x; } 
+}  
