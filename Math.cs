9de678796bd73450cff6fe0af93b1799c387fef0
mohs <morshedi.mohsen@gmail.com>
2017-02-18 17:13:00 -0500

    Adding Abs

--- Math.cs
+++ Math.cs
@@ -1,3 +1,4 @@
 public static int Mult(int x,int y) { return x * y; } 
 public static int Add(int x,int y) { return x + y; } 
 public static int Divide(int x,int y) { return x / y; } 
+public static int Abs(int x) { return x  ? x : -x; } 
